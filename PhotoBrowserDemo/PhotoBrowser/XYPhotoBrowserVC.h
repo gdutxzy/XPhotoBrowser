2fc93a19aff21bb0e732ec56d8c63ed4d0f578e5
XZY <xiezongyuan@xhg.com>
2018-09-12 20:29:28 +0800

    千里之行，始于足下

---
+++ PhotoBrowserDemo/PhotoBrowser/XYPhotoBrowserVC.h
@@ -0,0 +1,13 @@
+//
+//  XYPhotoBrowserVC.h
+//  PhotoBrowserDemo
+//
+//  Created by XZY on 2018/9/12.
+//  Copyright © 2018年 xiezongyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYPhotoBrowserVC : UIViewController
+
+@end
---
+++ PhotoBrowserDemo/PhotoBrowser/XYPhotoBrowserVC.m
@@ -0,0 +1,37 @@
+//
+//  XYPhotoBrowserVC.m
+//  PhotoBrowserDemo
+//
+//  Created by XZY on 2018/9/12.
+//  Copyright © 2018年 xiezongyuan. All rights reserved.
+//
+
+#import "XYPhotoBrowserVC.h"
+
+@interface XYPhotoBrowserVC ()
+
+@end
+
+@implementation XYPhotoBrowserVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
