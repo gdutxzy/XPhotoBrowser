22fb1ee3a288f80ff4b9ececd43a539f2606dd73
XZY <xiezongyuan@xhg.com>
2018-09-30 14:59:45 +0800

    pod sdwebimage

--- PhotoBrowserDemo/ViewController.m
+++ PhotoBrowserDemo/ViewController.m
@@ -7,16 +7,34 @@
 //
 
 #import "ViewController.h"
+#import <UIImageView+WebCache.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
 
 @end
 
 @implementation ViewController
+- (IBAction)imageViewTap:(UITapGestureRecognizer *)sender {
+    UIImageView *imageView = sender.view;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *userAgent = [NSString stringWithFormat:@"%@/%@ (%@; iOS %@; Scale/%0.2f)", [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleExecutableKey] ?: [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleIdentifierKey], [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] ?: [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleVersionKey], [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemVersion], [[UIScreen mainScreen] scale]];
+    if (userAgent) {
+        if (![userAgent canBeConvertedToEncoding:NSASCIIStringEncoding]) {
+            NSMutableString *mutableUserAgent = [userAgent mutableCopy];
+            if (CFStringTransform((__bridge CFMutableStringRef)(mutableUserAgent), NULL, (__bridge CFStringRef)@"Any-Latin; Latin-ASCII; [:^ASCII:] Remove", false)) {
+                userAgent = mutableUserAgent;
+            }
+        }
+        [[SDWebImageDownloader sharedDownloader] setValue:userAgent forHTTPHeaderField:@"User-Agent"];
+    }
+    
+    [self.imageView2 sd_setImageWithURL:[NSURL URLWithString:@"http://img5.imgtn.bdimg.com/it/u=1505624731,3616873916&fm=27&gp=0.jpg"]];
+
 }
 
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, "8.0"
+
+inhibit_all_warnings!
+# target的名字一般与你的项目名字相同
+target 'PhotoBrowserDemo' do
+  pod 'SDWebImage'
+end
